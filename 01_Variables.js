5a0049bc98f01a9603ceff4e50f06d8347748c2c
agadesh-softdev <agadesh.softdev@gmail.com>
2025-10-28 22:16:25 +0530

    updating variables in js

---
+++ 01_Variables.js
@@ -0,0 +1,31 @@
+/* There are three types of variables in JS
+01.let
+02.var
+03.const
+*/
+// ----------------------------------------------------------------------------------------------------------------
+
+let aga = 10;
+aga = 20;
+console.log(aga);
+
+var agi = 10;
+agi = 20;
+console.log(agi);
+
+/* In the const we can't reassign the values like agadesh=20 
+we cant re assign and re declare the values 
+*/
+const agadesh = 10;
+console.log(agadesh);
+
+// ----------------------------------------------------------------------------------------------------------------
+
+// in this we can't re declare the let like type:"let aki=20" but we type:"aki=20"
+let aki = 10;
+aki = 20;
+console.log(aki);
+
+var aka = 20;
+var aka = 30;
+console.log(aka);

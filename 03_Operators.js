5bc31295668b1e727cfbd239b5e4b8c838135759
agadesh-softdev <agadesh.softdev@gmail.com>
2025-11-06 21:06:39 +0530

    operators upload

---
+++ 03_Operators.js
@@ -0,0 +1,40 @@
+// Operators
+
+
+// 01 ARITHMETIC OPERATOR
+
+// ADDITION
+{
+  let a = 10;
+  let b = 20;
+  let c = a + b;
+  console.log(c);
+}
+
+//MULTIPLICATION
+{
+  let a = 30;
+  let c = 10;
+  console.log(a - c);
+}
+
+// DIVISION
+{
+  let a = 20;
+  let c = 2;
+  console.log(a / c);
+}
+
+//MODULUS
+{
+  let a = 11;
+  let b = 2;
+  console.log(a % b);
+}
+
+//EXPONENTIATION
+{
+  let a = 10;
+  let b = 2;
+  console.log(a ** b);
+}

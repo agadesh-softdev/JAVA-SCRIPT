e4c3822d8e9fcd1eaa2a83974351381e01fd27a5
agadesh-softdev <agadesh.softdev@gmail.com>
2025-11-12 21:57:06 +0530

    upload 05 operators in js

---
+++ 05_ControlFlows.js
@@ -0,0 +1,63 @@
+//===============================FOR LOOP==================================
+
+// Print 1 to 5 using for loop
+
+{
+  let i;
+  for (i = 1; i <= 5; i++) {
+    console.log(i);
+  }
+}
+
+// print 5 to 1 using for loop
+
+{
+  let aga;
+  for (aga = 5; aga >= 1; aga--) {
+    console.log(aga);
+  }
+}
+
+//===============================WHILE LOOP=================================
+
+// print 1 to 5 using while loop
+
+{
+  let i = 1;
+  while (i <= 5) {
+    console.log(i);
+    i++;
+  }
+}
+
+// print 5 to 1 using while loop
+
+{
+  let i = 5;
+  while (i >= 1) {
+    console.log(i);
+    i--;
+  }
+}
+
+//===============================DO-WHILE LOOP===========================
+
+// print 1 to 5 using do-while loop
+
+{
+  let i = 1;
+  do {
+    console.log(i);
+    i++;
+  } while (i <= 5);
+}
+
+// print 5 to 1 using do-while loop
+
+{
+  let i = 5;
+  do {
+    console.log(i);
+    i--;
+  } while (i >= 1);
+}

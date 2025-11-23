a11a09e3d438856701f20977cbac92ead90fefdc
agadesh-softdev <agadesh.softdev@gmail.com>
2025-11-23 14:44:41 +0530

    upload 06 file in js

---
+++ 06_Array.js
@@ -0,0 +1,15 @@
+buildings = ["school", "college", "home", "hospital"];
+console.log(buildings);
+buildings[1] = "airport";
+console.log(buildings);
+
+//=====================================================================================================================================
+
+college=["first year","second year","third year","fourth year"];
+console.log(college);
+college[3]="final year";
+console.log(college);
+college[2]="pre final year";
+console.log(college);
+
+

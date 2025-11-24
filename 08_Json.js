6ea5395dc608b7f3138a6727d89d6eab6ce7a81e
agadesh-softdev <agadesh.softdev@gmail.com>
2025-11-24 11:26:43 +0530

    upload 08 json file in js

---
+++ 08_Json.js
@@ -0,0 +1,14 @@
+// convert file into json(String)
+
+let info = {
+  name: "agadesh",
+  age: 20,
+  degree: "B.E",
+  branch: "CS",
+};
+
+const string_info = JSON.stringify(info);
+console.log(string_info);
+
+const parse_info = JSON.parse(string_info);
+console.log(parse_info);

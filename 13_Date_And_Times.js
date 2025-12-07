2cd6f4c7a2827346f491c42fcdfaf79e41811760
agadesh-softdev <agadesh.softdev@gmail.com>
2025-12-07 16:55:52 +0530

    update date and time in js

---
+++ 13_Date_And_Times.js
@@ -0,0 +1,30 @@
+{
+  let currentdate = new Date();
+  console.log(currentdate);
+}
+//----------------------------------------------------------------------------------------------------------------------------------
+
+{
+  let currentdate = new Date();
+  let year = currentdate.getFullYear();
+  let month = currentdate
+    .toLocaleString("default", { month: "short" })
+    .toUpperCase();
+  let day = currentdate.getDate();
+  let hours = currentdate.getHours();
+  let minutes = currentdate.getMinutes();
+  let seconds = currentdate.getSeconds();
+  let miliseconds = currentdate.getMilliseconds();
+  let mydate = "CURRENT DATE : " + day + "/" + month + "/" + year;
+  console.log(mydate);
+  let mytime =
+    "CURRENT TIME : " +
+    hours +
+    ":" +
+    minutes +
+    ":" +
+    seconds +
+    ":" +
+    miliseconds;
+  console.log(mytime);
+}

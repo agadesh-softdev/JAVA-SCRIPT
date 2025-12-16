ec22b18a99787aeca0b47e1e76925a8f268d78d4
agadesh-softdev <agadesh.softdev@gmail.com>
2025-12-16 18:56:28 +0530

    adding modules.js

---
+++ 14_Modules/module1.js
@@ -0,0 +1,9 @@
+import { log } from "node:console";
+
+export let num1 = 10;
+export var num2 = 20;
+export const num3 = 30;
+
+export default function aga() {
+  console.log("Fetching Details");
+}
---
+++ 14_Modules/module2.js
@@ -0,0 +1,7 @@
+import { num1, num2 as agi, num3 } from "./module1.js";
+import aga from "./module1.js";
+
+console.log(num1);
+console.log(agi);
+console.log(num3);
+aga();

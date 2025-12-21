f6fe5f8a4acf6ec532f493f3670698793e8a8131
agadesh-softdev <agadesh.softdev@gmail.com>
2025-12-21 14:35:58 +0530

    update callback

--- 18_Callback.js
+++ 18_Callback.js
@@ -54,3 +54,34 @@
   }
   login(show);
 }
+
+{
+  function products(callback) {
+    console.log("Fetching Product Details");
+    const names = {
+      id: 501,
+      product_name: "laptop",
+      price: 65000,
+      instock: true,
+    };
+    callback(names);
+  }
+  function person(product) {
+    console.log("Proudct ID :", product.id);
+    console.log("Proudct Name :", product.product_name);
+    console.log("Price :", product.price);
+    console.log("In stock :", product.instock);
+  }
+  products(person);
+}
+
+{
+  function num(a, back) {
+    result = a * 2;
+    back(result);
+  }
+  function aga(agi) {
+    console.log("The number is " + agi);
+  }
+  num(10, aga);
+}
